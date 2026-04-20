04363aac866fc5f5d20bad493f3745d8680e1222
Torry Chmelik <torry.chmelik@gmail.com>
2026-04-20 00:32:44 -0700

    Refactor player queries and fix function parameters

--- queries.py
+++ queries.py
@@ -1,28 +1,24 @@
 from db_python import connect_to_db
 
+
 def get_all_players():
     cnx = connect_to_db()
     cursor = cnx.cursor(dictionary=True)
 
     query = """
-
-    SELECT NodeID, Name
-    FROM Node
+    SELECT NodeID, Name, Sport, Position, pts, reb, ast
+    FROM Nodes
     WHERE NodeType = 'Player'
-
     """
-
     cursor.execute(query)
     results = cursor.fetchall()
-
+    cursor.close()
     cnx.close()
-
     return results
 
 
-
-def get_all_teammates(player):
-    cnx = connect_to_db
+def get_all_teammates(player_id):
+    cnx = connect_to_db()
     cursor = cnx.cursor(dictionary=True)
 
     query = """
@@ -34,12 +30,12 @@ def get_all_teammates(player):
         (e.TargetNodeID = %s AND n.NodeID = e.SourceNodeID)
     )
     WHERE e.EdgeType = 'teammate_of'
-
     """
 
+    cursor.execute(query, (player_id, player_id))
+    results = cursor.fetchall()
 
-    cursor.execute(query, (player, player))
-    results = cursor.fetchall
-
+    cursor.close()
     cnx.close()
+
     return results
