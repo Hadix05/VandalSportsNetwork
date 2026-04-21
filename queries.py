8b2f5ba242849939f9ae6fd00d9c1030b1f9a2a8
Torry Chmelik <torry.chmelik@gmail.com>
2026-04-20 20:25:32 -0700

    Add functions to retrieve players and teams from DB

--- queries.py
+++ queries.py
@@ -39,3 +39,57 @@ def get_all_teammates(player_id):
     cnx.close()
 
     return results
+
+def get_players_by_stats(min_pts=0, min_reb=0, min_ast=0):
+    cnx = connect_to_db()
+    cursor = cnx.cursor(dictionary=True)
+    query = """
+    SELECT NodeID, Name, Sport, pts, reb, ast
+    FROM Nodes
+    WHERE NodeType = 'Player'
+      AND pts >= %s
+      AND reb >= %s
+      AND ast >= %s
+    ORDER BY pts DESC
+    """
+    cursor.execute(query, (min_pts, min_reb, min_ast))
+    results = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return results
+
+def get_players_by_team(team_name):
+    cnx = connect_to_db()
+    cursor = cnx.cursor(dictionary=True)
+    query = """
+    SELECT n.NodeID, n.Name, n.Sport, n.pts, n.reb, n.ast
+    FROM Nodes n
+    JOIN Edges e ON n.NodeID = e.SourceNodeID
+    JOIN Nodes team ON e.TargetNodeID = team.NodeID
+    WHERE n.NodeType = 'Player'
+      AND e.EdgeType = 'plays_for'
+      AND team.NodeType = 'Team'
+      AND team.Name = %s
+    ORDER BY n.pts DESC
+    """
+    cursor.execute(query, (team_name,))
+    results = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return results
+
+def get_all_teams():
+    cnx = connect_to_db()
+    cursor = cnx.cursor(dictionary=True)
+    query = """
+    SELECT NodeID, Name, Sport
+    FROM Nodes
+    WHERE NodeType = 'Team'
+    ORDER BY Name ASC
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return results
+
